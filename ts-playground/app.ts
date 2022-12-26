fcebe74e7db11d60d8b5defdae09440a9daf901c
sbpark88 <saebyulpark88@gmail.com>
2022-12-26 23:20:36 +0900

    Add: TypeScript Playground with Node.js

---
+++ ts-playground/app.ts
@@ -0,0 +1,21 @@
+// @ts-ignore
+import express from "express";
+import router from "./router/index";
+import * as ejs from "ejs";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Playground app listening at http://localhost:${port}`)
+})
+
+app.use(express.static("public"));
+app.use(express.static("router"));
+app.use("/scripts", express.static("node_modules"));
+app.use("/js", express.static("dist"));
+app.disable("etag");
+app.set("views", "/views");
+app.set("view engine", "ejs");
+app.engine("ejs", ejs.renderFile);
+app.use(router);
---
+++ ts-playground/router/index.ts
@@ -0,0 +1,24 @@
+// @ts-ignore
+import express, {response} from "express";
+import * as path from "path";
+
+const router = express.Router();
+const __dirname = path.resolve();
+
+export default router;
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "main.html"))
+})
+
+router.get("/playground1", (req, res) => {
+    res.render(path.join(__dirname, "views", "group1/playground1.ejs"))
+})
+
+router.get("/playground2", (req, res) => {
+    res.render(path.join(__dirname, "views", "group1/playground2.ejs"))
+})
+
+router.get("/playground3", (req, res) => {
+    res.render(path.join(__dirname, "views", "group2/playground3.ejs"))
+})
